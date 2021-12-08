d85834e9a950b0623ee40c19757a216617be901d
Инара <inarashugaikina@gmail.com>
2021-12-08 15:22:42 +0600

    Change app name in configuration

--- parking_systems/apps.py
+++ parking_systems/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ParkingSystemConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'parking_system'
+    name = 'parking_systems'
