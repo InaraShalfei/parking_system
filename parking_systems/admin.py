9fbff13dbfcf3bd1747bb68ebf08e69bdab4276d
Инара <inarashugaikina@gmail.com>
2021-12-08 16:41:51 +0600

    Models registration in admin zone

--- parking_systems/admin.py
+++ parking_systems/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from parking_systems.models import Parking, Reservation
+
+
+class ReservationAdmin(admin.ModelAdmin):
+    list_filter = ('start_time', )
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(Reservation, ReservationAdmin)
+admin.site.register(Parking)
