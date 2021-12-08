f8e95c3f63344c97c7cd927dcfa3a8cb9b975af3
Инара <inarashugaikina@gmail.com>
2021-12-08 17:09:19 +0600

    Rendering list of parking slots onto the main page

--- parking_systems/views.py
+++ parking_systems/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from parking_systems.models import Parking
+
 
 def index(request):
-    return render(request, 'parking_systems/index.html')
+    parking_slots = Parking.objects.all()
+    return render(request, 'parking_systems/index.html', {'parking_slots': parking_slots})
