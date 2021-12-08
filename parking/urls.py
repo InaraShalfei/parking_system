fd16b651fc479d74ec95c8f5db9bc365fbf27030
Инара <inarashugaikina@gmail.com>
2021-12-08 15:28:07 +0600

    Changes based on change of app name

--- parking/urls.py
+++ parking/urls.py
@@ -4,10 +4,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('auth/', include('users.urls', namespace='users')),
     path('auth/', include('django.contrib.auth.urls')),
-    path('', include('parking_system.urls', namespace='parking_systems')),
+    path('admin/', admin.site.urls),
+    path('', include('parking_systems.urls', namespace='parking_systems')),
 ]
 if settings.DEBUG:
     urlpatterns += static(
