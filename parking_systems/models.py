da58cbe9dce6f44d6d8ac2d6cec06252121e5475
Инара <inarashugaikina@gmail.com>
2021-12-11 18:31:41 +0600

    Fixing html design

--- parking_systems/models.py
+++ parking_systems/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Parking(models.Model):
 
     def __str__(self):
-        return f'Парковочное место № {self.id}'
+        return f'Парковочное место №{self.id}'
 
 
 class Reservation(models.Model):
